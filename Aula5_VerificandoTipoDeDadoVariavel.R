92e2719e035e19f7bf8fb12355c62e145d479d1f
Francisco Chaves <francisco.jschaves@gmail.com>
2018-02-28 12:02:42 -0300

    Verificando o tipo de dado da variável

---
+++ Aula5_VerificandoTipoDeDadoVariavel.R
@@ -0,0 +1,21 @@
+#Aula 5 - Verificacao de tipo de dado
+
+
+#funcoes is.[tipo de dado]([variavel a ser verificada]) -> TRUE ou FALSE
+
+variavel <- "teste"
+
+# is.numeric - se he ponto flutuante
+is.numeric(variavel)
+
+# is.integer - se he inteiro
+is.integer (variavel)
+
+# is.logical - se he um verdadeiro ou falso
+is.logical (variavel)
+
+# is.character - se he texto
+is.character (variavel)
+
+# is.double - se he numero dupla precisao
+is.double (variavel)
