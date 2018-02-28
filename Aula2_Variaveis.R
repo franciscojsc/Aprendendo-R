933c3e221b328fb23cd583aa6a914cb87b858018
Francisco Chaves <francisco.jschaves@gmail.com>
2018-02-28 11:07:21 -0300

    Utilizando variáveis

---
+++ Aula2_Variaveis.R
@@ -0,0 +1,17 @@
+# Variáveis
+
+#Atribuindo valores / criando variáveis
+
+variavel <- 10
+variavel  # valor : 10
+
+#Atribuir outro tipo de valor na variável "variavel"
+variavel <- "pastel"
+variavel
+
+#Resultado de um cálculo
+
+x <- 20 ^ 2
+
+x / 4
+
