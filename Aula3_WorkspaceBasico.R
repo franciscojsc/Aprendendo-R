1cfc557dd91907198889714ecf5ef0b57c35eba3
Francisco Chaves <francisco.jschaves@gmail.com>
2018-02-28 11:38:22 -0300

    Operações de limpar e apagar objetos do workspace

---
+++ Aula3_WorkspaceBasico.R
@@ -0,0 +1,17 @@
+#Aula 3 - Workspace (Básico)
+
+#Criar variável x
+
+x <- 0
+
+#Criando outra variavel
+varialveWorkspace <- "exemplo"
+
+#Listar os objetos no workspace
+ls()
+
+#Apagar objeto/variavel do workspace
+rm([nome da variavel])
+
+#Apagar todos os objetos/variaveis do workspace
+rm(list = ls())
