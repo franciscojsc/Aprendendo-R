2a58ee9596d2db10f358790cb2224fb4c687e81b
Francisco Chaves <francisco.jschaves@gmail.com>
2018-02-28 09:21:03 -0300

    Operações Matemática básica

---
+++ Aula1_OperacoesMatematicasBasica.R
@@ -0,0 +1,22 @@
+#Operações básicas Matematica
+
+#Adição
+4 + 5
+
+#Subtração
+4 - 5
+
+#Divisão
+4 / 6
+
+#Multiplicação
+4 * 6
+
+#Exponenciação
+4 ^ 4
+
+#Resto da divisão
+2 %% 4
+
+#Ordem de prioridade
+2 * (2 + 1)
