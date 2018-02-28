12fe478779cff85c3a8875f787029643204ff617
Francisco Chaves <francisco.jschaves@gmail.com>
2018-02-28 11:48:07 -0300

    Exibir o tipo de dado

---
+++ Aula4_TipoDeDados.R
@@ -0,0 +1,24 @@
+#Aula 4 - Tipos básicos de dados
+
+#(Numeros)
+#Inteiro - Integer
+4
+class(4L) 
+#Sempre colocar o L na frente do número para o R entender que é um numero inteiro
+
+#Ponto flutuante - Numeric
+4.3
+class(65.99999)
+
+#Caractere - character
+"Pastel"
+class("Pastel")
+
+#Logico - Logico TRUE ou FALSE  / T ou F
+class(TRUE) #logical
+class(FALSE) #logical
+class(t)  #function
+class(T)  #logical
+class(f)  #Nao encontrado
+class(F)  #logical
+
