ff0e24fe27cf6b48fb2590e1181721cceeb34138
Francisco Chaves <francisco.jschaves@gmail.com>
2018-03-05 17:09:35 -0300

    Conversão de dados
    Utilizado para converter um tipo de dado em outro tipo.

---
+++ Aula6_ConversaoDados.R
@@ -0,0 +1,31 @@
+#Aula 6 - Conversão (coersão) de dados
+
+#ambiente
+varC <- "5"
+varI <- 5
+varN <- 4.5
+varL <- "TRUE"
+
+#Converter (coersão) do dado
+
+#as.integer - inteiro
+
+as.integer(varC)
+as.integer(varI)
+as.integer(varN)
+as.integer(varL)
+
+#as.character
+
+as.character(varC)
+as.character(varI)
+as.character(varN)
+as.character(varL)
+
+#as.logical
+
+as.logical(varC)
+as.logical(varI)
+as.logical(varN)
+as.logical(varL)
+
