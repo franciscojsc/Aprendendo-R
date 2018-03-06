47b63baefa3b6a3339bd050f93689dda4c7b4447
Francisco Chaves <francisco.jschaves@gmail.com>
2018-03-06 15:17:28 -0300

    Operações matématicas em vetores

---
+++ Aula10_OperacoesMatematicaEmVetores.R
@@ -0,0 +1,31 @@
+# Operações matématicas em vetores
+
+# Vetor a ser calculado
+x <- c(1:10)
+
+#Soma
+x + 10
+
+#Multiplicação
+x * 10
+
+# Multiplicação entre vetores
+mult <- c(2:12)
+
+x * mult # O comprimento tem que ser igual ou múltiplo
+
+mult <- c(2:11)
+x
+mult
+x * mult
+
+# Vetor menor que o alvo
+y <- c(1:3)
+x * y
+
+# Ajustar
+y <- c(2,3)
+x
+y
+x * y
+
