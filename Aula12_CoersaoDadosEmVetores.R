98052dc08df56ea891114b597b371e03e2fb438d
Francisco Chaves <francisco.jschaves@gmail.com>
2018-03-08 12:39:03 -0300

    Coerção de dados em vetores
    Vetores só pode ter um tipo de dados

---
+++ Aula12_CoersaoDadosEmVetores.R
@@ -0,0 +1,28 @@
+# Coerção de dados em vetores
+
+numeros <- c(1:3)
+class(numeros)
+
+#Atribuindo novo valor ao números
+numeros <- c(numeros, 3.3)
+numeros
+
+class(numeros[1])
+
+#Vetor só pode ter um tipo de dado
+numeros <- c(numeros, 4L)
+numeros
+
+#Adicionando strings no vetor
+numeros <- c(numeros, "18") #Converte todos os elementos em caractere
+
+numeros <- c(numeros, 90)
+
+#Atribuir valor Logical no vetor numérico
+numeros <- c(numeros, TRUE)
+numeros <- c(numeros, FALSE)
+
+logicos <- c(TRUE, FALSE, TRUE)
+class(logicos[1])
+
+logicos <- c(logicos, 90)
