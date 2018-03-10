0387862271eb32e184cf69d9976fb3e8eda8ed7d
Francisco Chaves <francisco.jschaves@gmail.com>
2018-03-09 23:58:45 -0300

    Utilizando o Factor, para agrupar classes

---
+++ Aula20_ UtilizandoFactors.R
@@ -0,0 +1,9 @@
+# Factors
+
+vetorJogadores <- c("BR","BR","BR","BR","BR","ARG","ARG","ARG","ARG","CHI","CHI","PAR","PAR")
+
+factorJogadores <- factor(vetorJogadores)
+factorJogadores
+str(factorJogadores)
+
+summary(factorJogadores)
